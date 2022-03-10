c2f01cb30c2464982b49e588fe1ab5ae7b9063c9
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-03-10 12:06:20 -0600

    set up database structure with models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int numOne = 5;
+        double numTwo = 19.99;
+        double sum = numOne + numTwo;
+        System.out.println(sum);
+
+        String myString = "foobar";
+        String concat = numOne + " " + myString;
+        System.out.println(concat);
+    }
+}
+
+// PICK UP ON LESSON 2 AT CREATE A TESTMODEL.DEMO CLASS
